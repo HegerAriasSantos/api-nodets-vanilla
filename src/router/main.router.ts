faf9b800c2967ac3cbe53dfdcccaa82f7a8b9935
Heger Arias Santos <hegel.0218@gmail.com>
2023-01-15 02:10:04 -0400

    little bug fixed

--- src/router/main.router.ts
+++ src/router/main.router.ts
@@ -10,7 +10,7 @@ export default class MainRouter implements IRouter {
 		const entity = req.url?.split("/")[1] || "";
 		const resoponse = new Response();
 		if (!entity || !routes.hasOwnProperty(entity))
-			return resoponse.error(res, "Not found", 404);
+			return resoponse.error(res, "Entity Not Found", 404);
 
 		routes[entity].manageRoutes(req, res);
 	}
